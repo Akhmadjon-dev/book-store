6a873f67aa9917fa1af7f0266086f4ded3d73b0b
akhmadjon-dev <info.ahmadjon@gmail.com>
2022-02-04 21:42:32 +0500

    redirect implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import SignUp from './container/Auth/SignUp.jsx'
+import React, {Fragment} from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+
+import SignUp from './container/Auth/SignUp'
+import SignIn from './container/Auth/SignIn'
+
 
 function App() {
   return (
-    <div>
-      <SignUp /> 
-    </div>
+    <Fragment>
+      <Routes>
+        <Route path="/" element={<h3>Home</h3>} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/not-found" element={<h3>Not Found</h3>} />
+        <Route path="*" element={<Navigate to="/not-found" />} />
+      </Routes>
+    </Fragment>
   )// comment
 }
 
