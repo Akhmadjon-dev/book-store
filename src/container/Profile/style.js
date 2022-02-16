154dc46873141f0db41ed6bda47321b2a087d089
Javlon <59303287+javlona@users.noreply.github.com>
2022-02-15 23:19:46 -0600

    profile ui added

--- src/container/Profile/style.js
+++ src/container/Profile/style.js
@@ -2,7 +2,26 @@ import styled from 'styled-components'
 import color from '../../styles/colors'
 
 const Container = styled.div`
-
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 20px 20px;
+    label {
+        color: #fff;
+    }
+    img {
+        width: 200px;
+        border-radius: 50%;
+    }
+    .profile__my-account {
+        display: flex;
+        justify-content: center;
+        .profile__img {
+            width: 35%;
+        }
+        .profile__form {
+            width: 55%;
+        }
+    }
 
 `
 
