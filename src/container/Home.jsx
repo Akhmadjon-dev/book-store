baaf0ba5ae4ab808b8bec1afc60679339addcf53
Javlon <59303287+javlona@users.noreply.github.com>
2022-02-17 21:48:33 -0600

    search

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Hero  from '../components/Hero';
 import Search from '../components/Search';
 import Books from '../container/Books';
 import Authors from '../container/Authors';
-
+import client from "../utils/axios";
 
 
 function Home() {
 
-  const searchHandler = (value) => {
-    
+  const [searchData, setSearchData] = useState()
+
+  const searchHandler = async (value) => {
+    try{
+      const res = await client.get('/books/filter/'+value)
+      console.log('category', res)
+      setSearchData(res.data.payload)
+    }
+    catch (err) {
+      console.log(err)
+    }
   }
 
   return (
     <div>
-        <Hero />
-        <Search />
+        <Hero  />
+        <Search searchHandler={searchHandler} />
         <Routes>
           <Route path="/books" element={<Books />} />
           <Route path="/authors" element={<Authors />} />
